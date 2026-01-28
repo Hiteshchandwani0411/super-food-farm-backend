cb8be41393a01b4e011f9ff23d18bdc1d6731fcd
pa526 <p7266634@gmail.com>
2026-01-28 23:27:45 +0530

    layout-updation

---
+++ Super-Food-Farm-website/src/app/features/auth/pages/home-page/home-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Layout } from '../../../../shared/components/layout/layout';
+
+@Component({
+  selector: 'app-home-page',
+  imports: [Layout],
+  templateUrl: './home-page.html',
+  styleUrl: './home-page.css',
+})
+export class HomePage {
+
+}
---
+++ Super-Food-Farm-website/src/app/shared/components/layout/layout.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Header } from '../header/header';
+import { Footer } from '../footer/footer';
+
+@Component({
+  selector: 'app-layout',
+  imports: [Header, Footer],
+  templateUrl: './layout.html',
+  styleUrl: './layout.css',
+})
+export class Layout {
+
+}
