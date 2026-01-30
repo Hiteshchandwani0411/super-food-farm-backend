615de8ea3bf299a7786828282684639dc421f069
pa526 <p7266634@gmail.com>
2026-01-30 09:49:34 +0530

    added new components

---
+++ Super-Food-Farm-website/src/app/shared/components/hero-section/hero-section.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-section',
+  imports: [],
+  templateUrl: './hero-section.html',
+  styleUrls: ['./hero-section.css','../../../../assets/styles/theme.css'],
+})
+export class HeroSection {
+
+}
